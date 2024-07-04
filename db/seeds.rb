a1744276b115c72fc952961ea369953faf701f02
Nicolas Nasri <nicolas.lrge@gmail.com>
2024-07-04 15:53:34 +0200

    seed terminé

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,17 @@
 require "open-uri"
 require "json"
 
+Movie.destroy_all
 url = "https://tmdb.lewagon.com/movie/top_rated."
 top_rated = URI.open(url).read
 movies = JSON.parse(top_rated)
 
 movies["results"].each do |m|
-  Movie.create(title: m["title"], overview: m["overview"], rating: m["vote_average"])
+  poster_path = m["poster_path"]
+  Movie.create!(
+    title: m["title"],
+    overview: m["overview"],
+    rating: m["vote_average"],
+    poster_url: "https://image.tmdb.org/t/P/w500#{poster_path}"
+  )
 end
