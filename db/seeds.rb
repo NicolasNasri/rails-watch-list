20e7f14971c66a636f65d6ec72dbf0a1afb4081f
Nicolas Nasri <nicolas.lrge@gmail.com>
2024-07-04 15:31:51 +0200

    modèles terminés

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "open-uri"
+require "json"
+
+url = "https://tmdb.lewagon.com/movie/top_rated."
+top_rated = URI.open(url).read
+movies = JSON.parse(top_rated)
+
+movies["results"].each do |m|
+  Movie.create(title: m["title"], overview: m["overview"], rating: m["vote_average"])
+end
